1893501bec2cf8f9cf72fd69c1e0c6db0a913733
Tianer Zhou <ezhoureal@gmail.com>
2025-04-22 17:16:53 +0800

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Tianer Zhou on 2025/4/22.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ playgroundTests/playgroundTests.swift
@@ -0,0 +1,16 @@
+//
+//  playgroundTests.swift
+//  playgroundTests
+//
+//  Created by Tianer Zhou on 2025/4/22.
+//
+
+import Testing
+
+struct playgroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

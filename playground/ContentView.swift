b3bdd34993af694a24e6cfe3e800f3ee5561b9f6
Tianer Zhou <ezhoureal@gmail.com>
2025-04-29 14:04:13 +0800

    Try a chatbot UI

--- playground/ContentView.swift
+++ playground/ContentView.swift
@@ -7,18 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Message: Identifiable {
+    let id = UUID()
+    let content: String
 }
 
+struct ContentView: View {
+    @State private var scroller: ScrollPosition = .init()
+    @State private var content = ""
+    @State private var texts = [Message(content: "Hello from the chatbot")]
+    
+    func submitText() {
+        if content == "" {
+            return
+        }
+        texts.append(Message(content: content))
+        content = ""
+        scroller.scrollTo(edge: .bottom)
+    }
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                let _ = print("ContentView")
+                ForEach(texts) { text in
+                    Text(text.content)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+        }.frame(width: 500, height: 200)
+            .scrollPosition($scroller)
+        HStack {
+            TextField("This is title", text: $content).border(Color.green)
+                .onSubmit {
+                submitText()
+            }
+            Button("Send") {
+                submitText()
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
